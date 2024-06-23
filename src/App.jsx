e90629c19383360d950814dc83d35925f0e8c9de
Giuseppe Frattura <giuseppefrattura@gmail.com>
2024-06-23 18:03:54 +0200

    Everthing is working before react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import HomeCards from './components/HomeCards'
 import JobListings from './components/JobListings'
+import ViewAllJobs from './components/ViewAllJobs'
 
 var titleText='Become a React Dev';
 var subtitleText='Find the React job that fits your skills and needs';
@@ -10,21 +11,10 @@ var subtitleText='Find the React job that fits your skills and needs';
 const App = () => {
   return (<>
     <Navbar />
-
     <Hero title={titleText} subtitle={subtitleText} />
-
     <HomeCards />
-
     <JobListings />
-
-
-  <section className="m-auto max-w-lg my-10 px-6">
-    <a
-      href="jobs.html"
-      className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-      >View All Jobs</a
-    >
-  </section>
+    <ViewAllJobs />
   </>
   )
 }
