e20772369581ac6ba4fc2ef458847950e327217d
Giuseppe Frattura <giuseppefrattura@gmail.com>
2024-06-23 19:03:47 +0200

    Introduction to react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import HomeCards from './components/HomeCards'
-import JobListings from './components/JobListings'
-import ViewAllJobs from './components/ViewAllJobs'
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from 'react-router-dom';import Navbar from './components/Navbar'
+import MainLayout from './layouts/MainLayout';
+import HomePage from './pages/HomePage';
+import JobsPage from './pages/JobPage';
 
-var titleText='Become a React Dev';
-var subtitleText='Find the React job that fits your skills and needs';
- 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout/>}>
+      <Route index element={<HomePage/>} /> 
+      <Route path='/jobs' element={<JobsPage/>} /> 
+    </Route> 
+)
+);
+
 const App = () => {
-  return (<>
-    <Navbar />
-    <Hero title={titleText} subtitle={subtitleText} />
-    <HomeCards />
-    <JobListings />
-    <ViewAllJobs />
-  </>
-  )
+  return <RouterProvider router={router}/>;
 }
 
 export default App
